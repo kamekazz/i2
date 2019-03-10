4e37ae923cfd81f10d07ca26d65926c1eca87d86
manuel taveras <taveras78@gmail.com>
2019-03-10 16:41:21 -0400

    test2

---
+++ client/build/precache-manifest.cb044776eb187c84a0557051be4a01f7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1bfb7b1fcb255143a39f",
+    "url": "/static/js/main.1bfb7b1f.chunk.js"
+  },
+  {
+    "revision": "a3a00fde433edbc44a1d",
+    "url": "/static/js/1.a3a00fde.chunk.js"
+  },
+  {
+    "revision": "fc918e48d8c30a13af99a3c60a359dd5",
+    "url": "/index.html"
+  }
+];
