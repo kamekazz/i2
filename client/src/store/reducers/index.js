82cd5e999b034057a532122782722f61036cb8ad
manuel taveras <taveras78@gmail.com>
2019-03-10 16:18:52 -0400

    om

--- client/src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import transiton from './transiton';
-import moduler from './moduler';
-
-export default combineReducers({
-    transiton:transiton,
-    moduler:moduler
-});
