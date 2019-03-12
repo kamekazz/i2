7cf61328dcfa08e5692f248c2e9be0cf118e199e
manuel taveras <taveras78@gmail.com>
2019-03-12 04:39:02 -0400

    test

--- server.js
+++ server.js
@@ -42,20 +42,12 @@ app.use(cors({
 
 app.use('/api/accounts', require('./routes/user.routes'))
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+
   
 
 
 
 
-app.listen(process.env.PORT ,()=>{
-    console.log('node.js server is ruining........');
-})
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
